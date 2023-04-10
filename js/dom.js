ce595b2c618d9f3dc26b22181dc6e54a1cf735be
Rubén Luengo <rubololer@gmail.com>
2023-04-10 09:38:18 +0200

    eventos load, scroll, DOMcontentLoaded

--- js/dom.js
+++ js/dom.js
@@ -416,3 +416,38 @@ document.addEventListener("click", (e) => {
     e.preventDefault();
   }
 });
+
+/* MANEJO DEL BOM */
+window.addEventListener("resize", (e) => {
+  console.clear();
+  console.log("********Evento Resize********");
+  console.log(window.innerWidth);
+  console.log(window.innerHeight);
+  console.log(window.outerWidth);
+  console.log(window.outerHeight);
+  console.log(e);
+});
+
+window.addEventListener("scroll", (e) => {
+  console.clear();
+  console.log("********Evento SCROLL********");
+  console.log(window.scrollX);
+  console.log(window.scrollY);
+  console.log(e);
+});
+
+window.addEventListener("load", (e) => {
+  
+  console.log("********Evento LOAD********");
+  console.log(window.screenX);
+  console.log(window.screenY);
+  console.log(e);
+});
+
+document.addEventListener("DOMContentLoaded", (e) => {
+  
+  console.log("********Evento DOMContentLoaded********");
+  console.log(window.screenX);
+  console.log(window.screenY);
+  console.log(e);
+})
