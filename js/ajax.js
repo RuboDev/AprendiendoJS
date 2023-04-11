61d9f8d8259e98d17020a7f215144b234f34ce13
Rubén Luengo <rubololer@gmail.com>
2023-04-11 07:41:08 +0200

    axios + async-await

--- js/ajax.js
+++ js/ajax.js
@@ -89,28 +89,54 @@
 // })();
 
 // Usando librería Axios
+// (() => {
+//   const $axios = document.getElementById("axios"),
+//   $fragment = document.createDocumentFragment();
+
+//   axios.get("https://jsonplaceholder.typicode.com/user")
+//   .then(res =>{
+//     console.log(res);
+//     let json = res.data;
+
+//     json.forEach((el) => {
+//       const $li = document.createElement("li");
+//       $li.innerHTML = `${el.name} -- ${el.email} -- ${el.phone}`;
+//       $fragment.appendChild($li);
+//     });
+//     $axios.appendChild($fragment);
+//   })
+//   .catch(err =>{
+//     console.log(err.response);
+//     let message = err.response.statusText || "Ocurrió un error";
+//     $axios.innerHTML = `Error ${err.response.status}: ${message}`;
+//   })
+//   .finally(() => {
+//     console.log("Esto se ejecutara siempre");
+//   })
+// })();
+
+// Axios + async-await
 (() => {
-  const $axios = document.getElementById("axios"),
+  const $axiosAsync = document.getElementById("axios-async"),
   $fragment = document.createDocumentFragment();
 
-  axios.get("https://jsonplaceholder.typicode.com/user")
-  .then(res =>{
-    console.log(res);
-    let json = res.data;
-
-    json.forEach((el) => {
-      const $li = document.createElement("li");
-      $li.innerHTML = `${el.name} -- ${el.email} -- ${el.phone}`;
-      $fragment.appendChild($li);
-    });
-    $axios.appendChild($fragment);
-  })
-  .catch(err =>{
-    console.log(err.response);
-    let message = err.response.statusText || "Ocurrió un error";
-    $axios.innerHTML = `Error ${err.response.status}: ${message}`;
-  })
-  .finally(() => {
-    console.log("Esto se ejecutara siempre");
-  })
+  async function getData(){
+    try {
+      let res = await axios.get("https://jsonplaceholder.typicode.com/users"),
+        json = await res.data;
+        console.log(res, json);
+        json.forEach((el) => {
+          const $li = document.createElement("li");
+          $li.innerHTML = `${el.name} -- ${el.email} -- ${el.phone}`;
+          $fragment.appendChild($li);
+        });
+        $axiosAsync.appendChild($fragment);
+    } catch (err) {
+      let message = err.response.statusText || "Ocurrió un error";
+      $axiosAsync.innerHTML = `Error ${err.response.status}: ${message}`;
+    } finally {
+      console.log("Esto se ejecutara siempre");
+    }
+  }
+  getData();
 })();
