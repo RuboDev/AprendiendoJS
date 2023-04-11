3aca2d0ef31041e846540191c5aaa7c270651ad4
Rubén Luengo <rubololer@gmail.com>
2023-04-11 05:37:41 +0200

    AJAX with API Fetch

--- js/ajax.js
+++ js/ajax.js
@@ -1,36 +1,57 @@
-(() => { //AJAX usando el objeto XMLHttpRequest();
-  const xhr = new XMLHttpRequest(),
-  $xhr = document.getElementById("xhr"),
-  $fragment = document.createDocumentFragment();
+// (() => { //AJAX usando el objeto XMLHttpRequest();
+//   const xhr = new XMLHttpRequest(),
+//   $xhr = document.getElementById("xhr"),
+//   $fragment = document.createDocumentFragment();
 
   
-  xhr.addEventListener("readystatechange", (e) => {
-    if (xhr.readyState !== 4) return;
-    if (xhr.status >= 200 && xhr.status < 300) {
-      //console.log(xhr);
-      //console.log("Éxito");
-      //console.log(xhr.responseText);
-      //$xhr.innerHTML = xhr.responseText;
-      let json = JSON.parse(xhr.responseText);
-      //console.log(json);
+//   xhr.addEventListener("readystatechange", (e) => {
+//     if (xhr.readyState !== 4) return;
+//     if (xhr.status >= 200 && xhr.status < 300) {
+//       //console.log(xhr);
+//       //console.log("Éxito");
+//       //console.log(xhr.responseText);
+//       //$xhr.innerHTML = xhr.responseText;
+//       let json = JSON.parse(xhr.responseText);
+//       //console.log(json);
+//       json.forEach(el => {
+//         const $li = document.createElement("li");
+//         $li.innerHTML = `${el.name} -- ${el.email} -- ${el.phone}`;
+//         $fragment.appendChild($li);
+//       });
+//       $xhr.appendChild($fragment);
+//     }else{
+//       //console.log("error");
+//       /* console.log(xhr); si xhr.statusText es igual a {} valida a false
+//       console.log({} === true); //false */
+//       let message = xhr.statusText || "Ocurrió un error";
+//       $xhr.innerHTML = `Error ${xhr.status}: ${message}`;
+//     }
+    
+//   })
+//   xhr.open("GET", "https://jsonplaceholder.typicode.com/users");
+//   // xhr.open("GET", "assets/users.json");
+//   xhr.send();
+// })();
+
+// API Fetch
+(() => {
+  const $fetch = document.getElementById("fetch"),
+  $fragment = document.createDocumentFragment();
+  
+  fetch("https://jsonplaceholder.typicode.com/users")
+    .then((res) => res.ok ? res.json(): Promise.reject(res))
+    .then((json) => {
       json.forEach(el => {
         const $li = document.createElement("li");
         $li.innerHTML = `${el.name} -- ${el.email} -- ${el.phone}`;
         $fragment.appendChild($li);
       });
-      $xhr.appendChild($fragment);
-    }else{
-      //console.log("error");
-      /* console.log(xhr); si xhr.statusText es igual a {} valida a false
-      console.log({} === true); //false */
-      let message = xhr.statusText || "Ocurrió un error";
-      $xhr.innerHTML = `Error ${xhr.status}: ${message}`;
-    }
-    
+      $fetch.appendChild($fragment);
+    })
+  .catch(err => {
+    console.log("Estamos en el catch",err);
   })
-  xhr.open("GET", "https://jsonplaceholder.typicode.com/users");
-  // xhr.open("GET", "assets/users.json");
-  xhr.send();
+  .finally(()=>{
+    console.log("Esto se ejecutará independientemente del resultado de la promesa fetch");
+  })
 })();
-
-// API Fetch
