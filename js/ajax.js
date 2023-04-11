fc346609e20d8fa3f36639f5f26f061bdb964a1b
Rubén Luengo <rubololer@gmail.com>
2023-04-11 06:29:33 +0200

    usando libreria axios

--- js/ajax.js
+++ js/ajax.js
@@ -34,24 +34,83 @@
 // })();
 
 // API Fetch
-(() => {
-  const $fetch = document.getElementById("fetch"),
-  $fragment = document.createDocumentFragment();
+// (() => {
+//   const $fetch = document.getElementById("fetch"),
+//   $fragment = document.createDocumentFragment();
   
-  fetch("https://jsonplaceholder.typicode.com/users")
-    .then((res) => res.ok ? res.json(): Promise.reject(res))
-    .then((json) => {
-      json.forEach(el => {
-        const $li = document.createElement("li");
-        $li.innerHTML = `${el.name} -- ${el.email} -- ${el.phone}`;
-        $fragment.appendChild($li);
-      });
-      $fetch.appendChild($fragment);
-    })
-  .catch(err => {
-    console.log("Estamos en el catch",err);
+//   fetch("https://jsonplaceholder.typicode.com/users")
+//     .then((res) => res.ok ? res.json(): Promise.reject(res))
+//     .then((json) => {
+//       json.forEach(el => {
+//         const $li = document.createElement("li");
+//         $li.innerHTML = `${el.name} -- ${el.email} -- ${el.phone}`;
+//         $fragment.appendChild($li);
+//       });
+//       $fetch.appendChild($fragment);
+//     })
+//   .catch(err => {
+//     console.log("Estamos en el catch",err);
+//   })
+//   .finally(()=>{
+//     console.log("Esto se ejecutará independientemente del resultado de la promesa fetch");
+//   })
+// })();
+
+// (() => { // Fetch async
+//   const $fetchAsync = document.getElementById("fetch-async"),
+//   $fragment = document.createDocumentFragment();
+  
+//   async function getData(){
+//     try {
+//       let res = await fetch("https://jsonplaceholder.typicode.com/users"),
+//       json = await res.json();
+
+//       //if (!res.ok) throw new Error("");
+//       if (!res.ok) throw {status: res.status, statusText: res.statusText}
+
+//       json.forEach((el) => {
+//         const $li = document.createElement("li");
+//         $li.innerHTML = `${el.name} -- ${el.email} -- ${el.phone}`;
+//         $fragment.appendChild($li);
+//       });
+
+//       $fetchAsync.appendChild($fragment);
+
+//     } catch (error) {
+//       console.log("Estoy en el catch", error);
+//       let message = error.statusText || "Ocurrió un error";
+//       $fetchAsync.innerHTML = `Error ${error.status}: ${message}`;
+//     } finally {
+//       console.log("Esto se ejecutará independientemente del resultado de la promesa fetch");
+//     }
+//   }
+
+//   getData();
+// })();
+
+// Usando librería Axios
+(() => {
+  const $axios = document.getElementById("axios"),
+  $fragment = document.createDocumentFragment();
+
+  axios.get("https://jsonplaceholder.typicode.com/user")
+  .then(res =>{
+    console.log(res);
+    let json = res.data;
+
+    json.forEach((el) => {
+      const $li = document.createElement("li");
+      $li.innerHTML = `${el.name} -- ${el.email} -- ${el.phone}`;
+      $fragment.appendChild($li);
+    });
+    $axios.appendChild($fragment);
   })
-  .finally(()=>{
-    console.log("Esto se ejecutará independientemente del resultado de la promesa fetch");
+  .catch(err =>{
+    console.log(err.response);
+    let message = err.response.statusText || "Ocurrió un error";
+    $axios.innerHTML = `Error ${err.response.status}: ${message}`;
+  })
+  .finally(() => {
+    console.log("Esto se ejecutara siempre");
   })
 })();
